57fde4544df9f918f350cb59e0a59efd0c4028df
Jonathan <jonathan@digital.gov.mg>
2021-12-22 11:56:20 +0300

    Serialization

--- wsgi.py
+++ wsgi.py
@@ -4,17 +4,27 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate
 
+BASE_URL = '/api/v1'
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 from models import Product
+from schemas import many_product_schema
 
 migrate = Migrate(app, db)
 
 @app.route('/hello', methods=['GET'])
 def hello():
     return "Hello World!", 200
+
+@app.route(f'{BASE_URL}/products', methods=['GET'])
+def get_many_product():
+    products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
+    return many_product_schema.jsonify(products), 200
